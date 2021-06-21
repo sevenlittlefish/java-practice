c0abe55e73ac8b1575caf4d47b36eb190476aa5c
liuhongru <wsflhr123>
2021-06-21 09:55:19 +0800

    Thread interrupt

---
+++ src/base/core/concurrent/thread/ThreadInterruptTest.java
@@ -0,0 +1,30 @@
+package base.core.concurrent.thread;
+
+public class ThreadInterruptTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            long start = System.currentTimeMillis();
+            while (!Thread.interrupted()) {
+                System.out.println(Thread.currentThread().getName() + " start sleep/wait");
+                try {
+//                    Thread.sleep(2000);
+                    synchronized (Thread.currentThread()) {
+                        Thread.currentThread().wait(2000);
+                    }
+                } catch (InterruptedException e) {
+                    //直接捕获中断异常如sleep或wait，Thread.interrupted()依旧是未中断状态，需要手动设置中断
+                    Thread.currentThread().interrupt();
+                }
+            }
+            System.out.println("Time consume " + (System.currentTimeMillis() - start)+" ms");
+        });
+        t.start();
+
+        Thread.sleep(1000);
+        if (!t.isInterrupted()) {
+            System.out.println("Interrupt " + t.getName());
+            t.interrupt();
+        }
+    }
+}
